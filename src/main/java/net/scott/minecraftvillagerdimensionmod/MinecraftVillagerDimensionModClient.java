f54e8201fc738ee93ba274b323ce4bb58e69d346
Scott Renslow <scottcraft3@hotmail.com>
2024-08-13 20:01:20 -0700

    Setup Complete

---
+++ src/main/java/net/scott/minecraftvillagerdimensionmod/MinecraftVillagerDimensionModClient.java
@@ -0,0 +1,10 @@
+package net.scott.minecraftvillagerdimensionmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MinecraftVillagerDimensionModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
