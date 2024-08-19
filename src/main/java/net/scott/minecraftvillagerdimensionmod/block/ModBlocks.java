fb24869cc8ce3480ac793caba18250082c54fe11
Scott Renslow <scottcraft3@hotmail.com>
2024-08-18 20:25:36 -0700

    Added a loot table

--- src/main/java/net/scott/minecraftvillagerdimensionmod/block/ModBlocks.java
+++ src/main/java/net/scott/minecraftvillagerdimensionmod/block/ModBlocks.java
@@ -11,6 +11,7 @@ import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 import net.scott.minecraftvillagerdimensionmod.MinecraftVillagerDimensionMod;
 import net.minecraft.util.Identifier;
 
