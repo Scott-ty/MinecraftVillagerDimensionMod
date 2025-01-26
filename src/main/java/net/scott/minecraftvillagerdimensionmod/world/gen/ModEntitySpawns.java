a09c64d0570da2dc42d890b0d066a5c2c0967fd3
Scott-ty <107960297+Scott-ty@users.noreply.github.com>
2025-01-25 23:44:31 -0800

    Updated loottables and tried to get boss to spawn naturally, it did not work

---
+++ src/main/java/net/scott/minecraftvillagerdimensionmod/world/gen/ModEntitySpawns.java
@@ -0,0 +1,17 @@
+package net.scott.minecraftvillagerdimensionmod.world.gen;
+
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
+import net.minecraft.entity.SpawnGroup;
+import net.minecraft.entity.SpawnRestriction;
+import net.scott.minecraftvillagerdimensionmod.entity.ModEntities;
+import net.scott.minecraftvillagerdimensionmod.world.biome.ModBiomes;
+
+public class ModEntitySpawns {
+//    public static void addSpawns(){
+//        BiomeModifications.addSpawn(BiomeSelectors.includeByKey(ModBiomes.CONTESTED_BIOME), SpawnGroup.MISC
+//                ,ModEntities.PILLAGER_BRUTE_BOSS, 50, 1, 1);
+//
+//        SpawnRestriction.register(ModEntities.PILLAGER_BRUTE_BOSS, SpawnRestriction);
+//    }
+}
